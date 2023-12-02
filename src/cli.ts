db933c7e93b9afcf9c141b164bfee5d496d0d0ec
SkyOPG <nicknameopg1@gmail.com>
2023-12-02 12:46:39 +0000

    added build system (alpha)

--- src/cli.ts
+++ src/cli.ts
@@ -1,65 +1,14 @@
 #! /usr/bin/env node
 import fs from 'fs';
 import { SkyScriptErr } from './util/error.js';
-import { run } from './index.js';
+import { build, run } from './index.js';
 
 const opts = process.argv;
 opts.shift();opts.shift();
 const args = opts;
 const cmd = args[0];
-function launchProfile(accessToken?){
-    const lines = {
-        homeScreen: [
-            "Welcome to SkyScript's Profile system!",
-            "",
-            "[1] Append access token",
-            "[2] Download package",
-            "[3] Post package",
-            "[4] Delete package"
-        ],
-        packages: [
-            "Package Chooser",
-            "",
-        ],
-        post: [
-            "Package Posting utility",
-            ""
-        ],
-        access: [
-            "Login - Access Token",
-            "",
-            "You need to specify both the mirror and "
-        ]
-    }
-}
 
-async function parseArgs(){
-    const options = {
-        debug: false
-    };
-    args.forEach(arg => {
-        if(["-v", "--version"].includes(arg)){
-            console.log("SkyScript is on version: v1.1.0")
-            process.exit(0);
-        } else if(["-d", "--debug"].includes(arg)){
-            options["debug"] = true;
-        }
-    })
-    return options;
-}
-
-async function cli(){
-    await parseArgs().then(options => {
-        switch(cmd){
-            case "profile":
-                launchProfile();
-            default:
-                run(cmd, options["debug"])
-        }
-    });
-}
-
-cli()
+run("main.ss")
 
 /*
 const [node_exec, ss_path, file, ...args] = process.argv;
--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import { SkyScriptWarn } from "./util/warn.js";
 import { Library } from './lib/index.js';
 import yml from 'yaml'
 import { SkyScriptErr } from './util/error.js';
+import { Lexer } from './syntax/lexer.js';
 
 function readFile(input){
     if(!fs.existsSync(input))
@@ -15,13 +16,77 @@ function readFile(input){
     return fs.readFileSync(input).toString();
 }
 
+interface Codebase {
+    os: {
+        name: string;
+        cpu: {
+            minUsage: number;
+            maxUsage: number;
+            resS: {
+                cores: number;
+                threads: number;
+            }
+        }
+    }
+    code: {
+        encoding: "hex" | "base64",
+        code?: string;
+    },
+    executable: {
+        isExecutable: boolean;
+        ext: string[];
+        requirements: string[];
+        tests: string[];
+    }
+}
+
+const codebase: Codebase = {
+    os: {
+        name: "SkyScriptVM",
+        cpu: {
+            minUsage: 0.20,
+            maxUsage: 40.8,
+            resS: {
+                cores: 1,
+                threads: 1
+            }
+        }
+    },
+    code: {
+        encoding: "hex",
+    },
+
+    executable: {
+        isExecutable: true,
+        ext: ["skx"],
+        requirements: ["node", "npm", "ss"],
+        tests: [
+            "darwin",
+            "linux",
+            "win64"
+        ]
+    }
+    
+}
+
+export function build(path: string){
+    const lexer = new Lexer();
+    const parser = new Parser();
+    const input = lexer.Tokenize(readFile(path));
+    const program = parser.createAST(input);
+    codebase.code["code"] = JSON.stringify(program);
+    const buf =Buffer.from(JSON.stringify(codebase)).toString("hex");
+    fs.writeFileSync("output.skx", buf);
+}
+
 export async function run(path: string ,debug?: boolean){
+    const lexer = new Lexer();
     const parser = new Parser();
     const json = yml.parse(readFile("ss.yml"));
     const lib = new Library(json.packages);
     const env = lib.env;
     await lib.registerPacks().then(() => {
-        const input = readFile(path);
+        const input = lexer.Tokenize(readFile(path));
         const program = parser.createAST(input);
         const result = evaluate(program, env);
         if(debug)
@@ -31,19 +96,22 @@ export async function run(path: string ,debug?: boolean){
 
 export async function runmain(args: string[], path: string){
     const file = args.shift()
+    const lexer = new Lexer()
     if(!file?.endsWith(".ss")) throw "file is not a .ss skyscript file";
     try{
     const parser = new Parser();
     const env = createGlobalEnv();
-    const input = readFile(path);
+    const input = lexer.Tokenize(readFile(path));
     const program = parser.createAST(input);
-    /*const result =*/ evaluate(program, env);
+    const buf =Buffer.from(JSON.stringify(program)).toString("hex");
+    fs.writeFileSync("output.skx", buf);
+    /*const result =*/ //evaluate(program, env);
     //console.log(result)
     } catch(e: any) {
         new SkyScriptWarn(e);
     }
 }
-
+/*
 export function repl(){
     let DebugMode = false;
     const parser = new Parser();
@@ -68,9 +136,9 @@ export function repl(){
         if(DebugMode){
             console.log(program);
         } else {
-            /*const result =*/ evaluate(program, env);
+            /*const result =evaluate(program, env);
             //console.log(result)
         }
         }
     }
-}
+}*/
